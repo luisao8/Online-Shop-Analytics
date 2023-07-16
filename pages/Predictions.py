194cd740c383fec223e6e9632f9c17f6a1eab846
luisao8 <74673031+luisao8@users.noreply.github.com>
2023-07-16 10:34:45 +0200

    Update Predictions.py

--- pages/Predictions.py
+++ pages/Predictions.py
@@ -1,35 +1,35 @@
-# #https://luisao8-online-shop-analytics-webapp-dashboard-k431ov.streamlit.app/
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import plotly.express as px
-# import altair as alt
-#
-# pd.set_option('display.max_columns', None)
-#
-#
-#
-# #HEADER
-# st.title("Predictions")
-# st.write("Sales prediction:")
-# data = pd.read_pickle("Hola.pkl")
-# #######
-# data = data.set_index("Date")
-# data = data.resample('W-MON', origin='start_day', offset='1D').sum(numeric_only=True).reset_index()
-# fig = px.line(data, x='Date', y='Sales', title='Time Series with Range Slider and Selectors')
-#
-# fig.update_xaxes(
-#     rangeslider_visible=True,
-#     rangeselector=dict(
-#         buttons=list([
-#             dict(count=1, label="1m", step="month", stepmode="backward"),
-#             dict(count=6, label="6m", step="month", stepmode="backward"),
-#             dict(count=1, label="YTD", step="year", stepmode="todate"),
-#             dict(count=1, label="1y", step="year", stepmode="backward"),
-#             dict(step="all")
-#         ])
-#     )
-# )
-#
-#
-# st.plotly_chart(fig, use_container_width=True)
+#https://luisao8-online-shop-analytics-webapp-dashboard-k431ov.streamlit.app/
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import altair as alt
+
+pd.set_option('display.max_columns', None)
+
+
+
+#HEADER
+st.title("Predictions")
+st.write("Sales prediction:")
+data = pd.read_pickle("Hola.pkl")
+#######
+data = data.set_index("Date")
+data = data.resample('W-MON', origin='start_day', offset='1D').sum(numeric_only=True).reset_index()
+fig = px.line(data, x='Date', y='Sales', title='Time Series with Range Slider and Selectors')
+
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+
+st.plotly_chart(fig, use_container_width=True)
